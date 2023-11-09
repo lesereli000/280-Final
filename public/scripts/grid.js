61569d317bb71c52f11d1bf54a7d0290fae96e80
Sofia <castons@rose-hulman.edu>
2023-11-08 23:23:51 -0500

    updated grid view

--- public/scripts/grid.js
+++ public/scripts/grid.js
@@ -1,1 +1,23 @@
-// some random stuff
+const printCardBtn = document.getElementById("printCardBtn");
+const printSelectBtn = document.getElementById("printSelectBtn");
+const printAllBtn = document.getElementById("printAllBtn");
+
+
+printCardBtn.addEventListener("click", () => {
+    
+    var mywindow = window.open('', 'PRINT', 'height=400,width=600');
+
+    mywindow.document.write('<html><head><title>' + document.title  + '</title>');
+    mywindow.document.write('</head><body >');
+    mywindow.document.write('<h1>' + document.title  + '</h1>');
+    mywindow.document.write(document.getElementById(elem).innerHTML);
+    mywindow.document.write('</body></html>');
+
+    mywindow.document.close(); // necessary for IE >= 10
+    mywindow.focus(); // necessary for IE >= 10*/
+
+    mywindow.print();
+    mywindow.close();
+
+    return true;
+});
