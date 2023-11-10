48e645ab4df7330f267965483d461fa3b9b0415a
Sofia <castons@rose-hulman.edu>
2023-11-10 10:01:45 -0500

    editing grid view api

--- public/scripts/grid.js
+++ public/scripts/grid.js
@@ -29,14 +29,7 @@ function printExternal(divId) {
     document.body.innerHTML = originalContents;
 }
 
-function setPrintPage(prnThis){
- 
-    prnDoc = document.getElementById('info');
-    prnDoc[0].setAttribute('href', prnThis);
-    window.print();
-   }
-
-for (const subcat of subcats){
+/*for (const subcat of subcats){
     subcat.onclick = function() {
         this.classList.toggle("active");
         if (this.classList.contains("active")){
@@ -47,6 +40,64 @@ for (const subcat of subcats){
         }
         console.log(activeFilters);
     };
+}*/
+
+async function fetchData() {
+    try {
+        // let queryString = "";
+
+        // queryString += `range=${slider.value}&`;
+        
+        // if(activeFilters.length != 0){
+        //     queryString += `filters=[${activeFilters}]&`
+        // }
+    
+        // if(search.value){
+        //     queryString += `search=${search.value}`;
+        // }
+    
+        // console.log(queryString);
+    
+        // if(zip){
+        //     queryString += `zipcode=${zip}`;
+        // } else if (county) {
+        //     queryString += `county=${county}`;
+        // }
+        const response = await fetch(`http://localhost:3000/?zipcode=47803`);
+        const data = await response.json;
+        console.log(response);
+        return response;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
 
+async function displayCards() {
+    const container = document.querySelector('.container');
+    const data = await fetchData();
+
+    if (!data) {
+        return;
+    }
+
+    data.forEach(item => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const title = document.createElement('h1');
+        title.textContent = item.site_name;
+
+        const subtitle = document.createElement('h2');
+        subtitle.textContent = item.agency_name;
+
+        const body = document.createElement('p');
+        body.textContent = item.agency_desc;
+
+        card.appendChild(title);
+        card.appendChild(body);
+        container.appendChild(card);
+    });
+}
+
+displayCards();
 
