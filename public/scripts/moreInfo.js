375aa6d0be8705952e47b510120ece7192ce6d9c
rhit-mooretj <mooretj@rose-hulman.edu>
2023-11-10 15:24:20 -0500

    Last TODO

--- public/scripts/moreInfo.js
+++ public/scripts/moreInfo.js
@@ -1,6 +1,7 @@
 /* TODO: Make print view look really nice (table?) */
 /* TODO: Make it so you can export a CSV */
 /* TODO: Modal to decide between these */
+/* TODO: Choose div from cards that we want to display/print */
 
 function printContent(divId) {
     var printContents = document.getElementById(divId).innerHTML;
