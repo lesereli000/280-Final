1d440a9c636cc774ca1e4b770ff7161b10aea0f6
rhit-mooretj <mooretj@rose-hulman.edu>
2023-11-10 03:09:33 -0500

    API kinda closer

--- public/scripts/new api.js
+++ public/scripts/new api.js
@@ -26,7 +26,7 @@ app.get("/", (request, response) => {
             zipcode = filters.zipcode;
             county = filters.county;
 
-            const range = filters.range;
+            const range = Number(filters.range);
             delete filters.range;
 
             // TODO: implement search parameter
@@ -52,17 +52,30 @@ app.get("/", (request, response) => {
                 obj.latitude = coords.lat;
 
                 const distance = await getDistance(obj);
+
+                if (isNaN(distance)) {
+                    return false;
+                }
                 return distance <= range;
  
             });
             response.set('Access-Control-Allow-Origin', '*');
             response.send(filtered);
+            console.log("All done!")
         }
     });
 });
 
 async function getCoords(obj) {
     let coords = "";
+
+
+
+    var firstChar = obj.address_1.charAt(0);
+    if (!(firstChar <= '9' && firstChar >= '0')) {
+        coords = { lat: 0, lng: 0 };
+        return coords;
+    }
 
     let address = obj.address_1 + " " + obj.city + " " + obj.county + " " + obj.state_province;
     let filteredAddress = address.replaceAll(" ", "%20");
@@ -83,11 +96,17 @@ async function getDistance(obj) {
         var macroData = county;
     }
 
+    console.log(obj.latitude);
+    console.log(obj.longitude);
+    
     return fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${obj.latitude},${obj.longitude}&origins=${macroData}&units=imperial&key=AIzaSyBPtQdhjLymTBQq5kKId0mO1Wjq6vFh6PY`)
         .then(response => response.json())
         .then(data => {
+            if (data.rows[0].elements[0].status === "ZERO_RESULTS") {
+                return 1000;
+            }
             distance = data.rows[0].elements[0].distance.text;
             distance = distance.replace(" mi", "");
-            return distance;
+            return Number(distance);
         });
 }
